89f78a56a124a02ab482423f9a26d0219c7fcd76
Gregorio <gregorio.galindo9711@hotmail.com>
2025-10-07 09:48:12 -0600

    Se agrega el archivo principal de java

---
+++ arjusven/src/main/java/com/back/arjusven/ArjusvenApplication.java
@@ -0,0 +1,13 @@
+package com.back.arjusven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArjusvenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArjusvenApplication.class, args);
+	}
+
+}
---
+++ arjusven/src/test/java/com/back/arjusven/ArjusvenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.arjusven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArjusvenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
